664ff07eb437c5d4b1bee96aeec36335c2ceea4f
Stephanie Noel <snoel@carlipa.com>
2024-02-17 18:15:16 +0100

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "petite-plante",
+  base: "/petite-plante",
   plugins: [
     vue(),
   ],
